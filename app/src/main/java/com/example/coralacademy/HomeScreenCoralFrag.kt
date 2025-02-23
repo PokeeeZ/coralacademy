ae91ccbc3e3d66534c48b704b2dfabe1f3a81228
Poke <162466609+PokeeeZ@users.noreply.github.com>
2025-02-23 23:14:47 +0800

    Renaming names

--- app/src/main/java/com/example/coralacademy/HomeScreenCoralFrag.kt
+++ app/src/main/java/com/example/coralacademy/HomeScreenCoralFrag.kt
@@ -43,7 +43,7 @@ class HomeScreenCoralFrag : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         val view = inflater.inflate(R.layout.fragment_home_screen_coral, container, false)
         dateTV = view.findViewById(R.id.dateTV)
         scheduleCalendar = view.findViewById(R.id.scheduleCalendar)
